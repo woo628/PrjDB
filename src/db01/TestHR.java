0d85a8a384acc0b74a81a990dd7fcd598e705cc2
woo628 <duddn2500@gmail.com>
2026-04-15 17:50:53 +0900

    java db 데이터가져오기

--- src/db01/TestHR.java
+++ src/db01/TestHR.java
@@ -8,7 +8,30 @@ public class TestHR {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName(driver);
-		Connection conn = DriverManager.getConnection(url, id, driver);
+		Connection conn = DriverManager.getConnection(url, id, pwd);
+		
+		Statement stmt = conn.createStatement();
+		String sql = "select a.employee_id, a.first_name, a.last_name, b.department_name, a.phone_number "
+				+ "from employees a "
+				+ "join departments b "
+				+ "on a.department_id = b.department_id "
+				+ "order by a.employee_id";
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		while (rs.next() != false) {
+			System.out.print(rs.getString(1) + ' ');
+			System.out.print(rs.getString(2) + ' ');
+			System.out.print(rs.getString(3) + ' ');
+			System.out.print(rs.getString(4)+ ' ');
+			System.out.print(rs.getString(5));
+			System.out.println();
+		}
+		
+		
+		rs.close();
+		stmt.close();
+		conn.close();
 	}
 
 }
